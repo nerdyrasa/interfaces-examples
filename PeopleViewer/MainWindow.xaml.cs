3b0765439a8afa7cc4ee5fcec6cffff7a1265e1a
nerdyrasa <nerdyrasa@users.noreply.github.com>
2016-05-13 16:46:30 -0500

    Initial commit

---
+++ PeopleViewer/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using PersonLibrary;
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace PeopleViewer
+{
+    public partial class MainWindow : Window
+    {
+        PeopleRepository peopleRepo = new PeopleRepository();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConcreteFetchButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearListBox();
+
+            IEnumerable people;
+            people = peopleRepo.GetPeople();
+            foreach (var person in people)
+            {
+                PersonListBox.Items.Add(person);
+            }
+        }
+
+        private void InterfaceFetchButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearListBox();
+
+            IEnumerable people;
+            people = peopleRepo.GetPeople();
+            foreach (var person in people)
+            {
+                PersonListBox.Items.Add(person);
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearListBox();
+        }
+
+        private void ClearListBox()
+        {
+            PersonListBox.Items.Clear();
+        }
+    }
+}
